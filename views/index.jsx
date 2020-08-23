1a48dc8cb5e89a62e62404af7ff4f571cce8af5a
Tom's Personal <thomasbwsu@gmail.com>
2020-08-23 13:51:29 -0400

    Data props from server

--- views/index.jsx
+++ views/index.jsx
@@ -3,37 +3,36 @@ import PropTypes from 'prop-types';
 import e from 'express';
 
 export default class TodoBox extends React.Component {
-    render() {
-        return (
-            <div className="todoBox">
-                <h1>Todos</h1>
-                <TodoList />
-                <TodoForm />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="todoBox">
+        <h1>Todos</h1>
+        <TodoList data = {this.props.data} />
+        <TodoForm />
+      </div>
+    );
+  }
 }
 
 class TodoList extends React.Component {
   render() {
-      return (
-          <div className="todoList">
-              <table style={{border: "2px solid black;"}}>
-                <tbody>
-                  <Todo title="Shopping">Milk</Todo>
-                  <Todo title="Hair cut">13:00</Todo>
-                  <Todo title="Learn React">15:00</Todo>
-                </tbody>
-              </table>
-          </div>
-      );
+    var todo = this.props.data.map(obj => { return <Todo title={obj.title} key={obj.title}>{obj.detail}</Todo>});
+    return (
+      <div className = "todoList">
+        <table style={{border: "2px solid black;"}}>
+          <tbody>
+            {todo}
+          </tbody>
+        </table>
+      </div>
+    );
   }
 }
 
 class Todo extends React.Component {
   constructor(props) {
-      super(props);
-      this.state = {checked: false};
+    super(props);
+    this.state = {checked: false};
   }
 
   handleChange(e) {
@@ -42,13 +41,13 @@ class Todo extends React.Component {
 
   render() {
     return (
-        <tr>
-            <td style={style.tableContent}>
-                <input type="checkbox" checked={this.state.checked} onChange={this.handleChange}/>
-            </td>
-            <td style={{border: "1px solid black;"}}>{this.props.title}</td>
-            <td style={{border: "1px solid black;"}}>{this.props.children}</td>
-        </tr>
+      <tr>
+        <td style={style.tableContent}>
+          <input type="checkbox" checked={this.state.checked} onChange={this.handleChange}/>
+        </td>
+        <td style={{border: "1px solid black;"}}>{this.props.title}</td>
+        <td style={{border: "1px solid black;"}}>{this.props.children}</td>
+      </tr>
     );
   }
 }
@@ -68,6 +67,6 @@ class TodoForm extends React.Component {
 
 let style = {
   tableContent: {
-      border: "1px solid black;"
+    border: "1px solid black;"
   }
 };
